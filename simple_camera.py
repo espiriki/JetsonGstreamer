f6f2e6249e4deab275fdf88d7007e409e0ce4fc8
Jose Cazarin <joseespiriki@gmail.com>
2021-09-07 20:20:33 -0600

    Cleaning the code a bit

--- simple_camera.py
+++ simple_camera.py
@@ -17,29 +17,6 @@ flip_values = {
 }
 
 
-def on_message(bus: Gst.Bus, message: Gst.Message, loop: GObject.MainLoop):
-    mtype = message.type
-    """
-        Gstreamer Message Types and how to parse
-        https://lazka.github.io/pgi-docs/Gst-1.0/flags.html#Gst.MessageType
-    """
-    if mtype == Gst.MessageType.EOS:
-        print("End of stream")
-        pipeline.set_state(Gst.State.NULL)
-        loop.quit()
-
-    elif mtype == Gst.MessageType.ERROR:
-        err, debug = message.parse_error()
-        print(err, debug)
-        loop.quit()
-
-    elif mtype == Gst.MessageType.WARNING:
-        err, debug = message.parse_warning()
-        print(err, debug)
-
-    return True
-
-
 def load_labels(filename):
     with open(filename, 'r') as f:
         return [line.strip() for line in f.readlines()]
@@ -56,45 +33,43 @@ def setup_model(args, interpreter):
     # NxHxWxC, H:1, W:2
     height = input_details[0]['shape'][1]
     width = input_details[0]['shape'][2]
-    img = Image.open(args["image"]).resize((width, height))
 
-    print(img)
-
-    # add N dim
-    input_data = np.expand_dims(img, axis=0)
-
-    if floating_model:
-
-        print("Float model!")
-
-        input_data = (np.float32(input_data) -
-                    args["input_mean"]) / args["input_std"]
-
-    get_labels(interpreter, input_data, floating_model, height, width, False)
-
-    return (width,height)
+    return (width, height)
 
 
-def get_labels(interpreter, input_data, floating_model, height, width, convert):
+def extract_label(best_result_idx, labels):
+
+    split_string = labels[best_result_idx].split(",", 1)[0]
+
+    label = split_string.split(":", 2)[1]
+
+    return label
+
+
+def convert_to_float32(data, num_channels):
+
+    data = np.frombuffer(data, dtype=np.uint8)
+
+    data = np.reshape(
+        data, newshape=(1, height, width, num_channels))
+
+    data = (np.float32(data) * (1.0 / 127.5)) - 1.0
+
+    data = data.view('<f4')
+
+    return data
+
+
+def get_labels(interpreter, input_data, height, width, labels):
+
+    num_channels = 3
 
     input_details = interpreter.get_input_details()
     output_details = interpreter.get_output_details()
 
-    if convert:
+    float_data = convert_to_float32(input_data, num_channels)
 
-        deserialized_bytes = np.frombuffer(input_data, dtype=np.uint8)
-        
-        deserialized_x = np.reshape(deserialized_bytes, newshape=(1, height, width, 3))
-
-        deserialized_y = (np.float32(deserialized_x) - 127.5) / 127.5
-
-        deserialized_z = deserialized_y.view('<f4')
-
-        interpreter.set_tensor(input_details[0]['index'], deserialized_z)
-
-    else:
-
-        interpreter.set_tensor(input_details[0]['index'], input_data)
+    interpreter.set_tensor(input_details[0]['index'], float_data)
 
     start_time = time.time()
     interpreter.invoke()
@@ -103,21 +78,27 @@ def get_labels(interpreter, input_data, floating_model, height, width, convert):
     results = np.squeeze(output_data)
 
     top_k = results.argsort()[-5:][::-1]
-    labels = load_labels(args["label_file"])
 
     stop_time = time.time()
 
+    print('time: {:.3f} ms'.format((stop_time - start_time) * 1000))
+
     best_result = top_k[0]
 
-    split_string =  labels[best_result].split(",", 1)[0]
-
-    label =  split_string.split(":", 2)[1]
+    label = extract_label(best_result, labels)
 
     score = float(results[best_result])
-    
+
     return score, label
 
-def new_sample_callback(appsink, interpreter):
+
+def new_sample_callback(appsink, data):
+
+    interpreter = data[0]
+
+    overlay = data[1]
+
+    labels = data[2]
 
     sample = appsink.emit("pull-sample")
 
@@ -128,20 +109,19 @@ def new_sample_callback(appsink, interpreter):
     frmt_str = caps_format.get_value('format')
     video_format = GstVideo.VideoFormat.from_string(frmt_str)
 
-    w, h = caps_format.get_value('width'), caps_format.get_value('height')
-
-    c = utils.get_num_channels(video_format)
+    width, height = caps_format.get_value(
+        'width'), caps_format.get_value('height')
 
     ret, map_info = buffer.map(Gst.MapFlags.READ)
 
-    results = get_labels(interpreter[0], map_info.data, True, w, h, True)
+    results = get_labels(
+        interpreter, map_info.data, width, height, labels)
 
-    interpreter[1].set_property("text", "{} - {:.2f}%".format(results[1],results[0]*100))
+    overlay.set_property(
+        "text", "{} - {:.2f}%".format(results[1], results[0]*100))
 
     return Gst.FlowReturn.OK
 
-
-Gst.init(None)
 
 ap = argparse.ArgumentParser()
 
@@ -159,24 +139,7 @@ ap.add_argument(
     required=True)
 
 ap.add_argument(
-    '-i',
-    '--image',
-    required=True,
-    help='image to be classified')
-
-ap.add_argument(
-    '--num_threads', default=1, type=int, help='number of threads')
-
-
-ap.add_argument(
-    '--input_mean',
-    default=127.5, type=float,
-    help='input_mean')
-
-ap.add_argument(
-    '--input_std',
-    default=127.5, type=float,
-    help='input standard deviation')
+    '--num_threads', default=2, type=int, help='number of threads')
 
 ap.add_argument(
     '-l',
@@ -184,14 +147,16 @@ ap.add_argument(
     help='name of file containing labels',
     required=True)
 
-args = vars(ap.parse_args())
 
+args = vars(ap.parse_args())
 
 interpreter = tflite.Interpreter(model_path=args["model_file"])
 
 interpreter.allocate_tensors()
 
 width, height = setup_model(args, interpreter)
+
+Gst.init(None)
 
 # create pipeline object
 pipeline = Gst.Pipeline()
@@ -239,13 +204,19 @@ cairo = Gst.ElementFactory.make("cairooverlay")
 
 overlay = Gst.ElementFactory.make("textoverlay")
 
+nv_tee = Gst.ElementFactory.make("nvtee")
+
 overlay.set_property("font-desc", "Sans, 32")
 
-appsink.connect("new-sample", new_sample_callback, (interpreter, overlay))
+labels = load_labels(args["label_file"])
 
-my_format = "RGB"
+appsink.connect("new-sample", new_sample_callback,
+                (interpreter, overlay, labels))
 
-caps_str = "video/x-raw, width=(int){0}, height=(int){1}, format=(string){2}".format(width, height, my_format)
+scale = Gst.ElementFactory.make("videoscale")
+
+caps_str = "video/x-raw, width=(int){0}, height=(int){1}, format=(string){2}".format(
+    width, height, "RGB")
 
 appsink_caps = Gst.Caps.from_string(caps_str)
 
@@ -262,7 +233,6 @@ pipeline.add(camera_filter)
 pipeline.add(nv_vid_conv)
 pipeline.add(h_264_enc)
 pipeline.add(rtp_264_pay)
-pipeline.add(appsink)
 pipeline.add(tee)
 pipeline.add(queue_1)
 pipeline.add(queue_2)
@@ -270,6 +240,9 @@ pipeline.add(udp_sink)
 pipeline.add(cairo)
 pipeline.add(overlay)
 pipeline.add(videoconvert_1)
+pipeline.add(scale)
+pipeline.add(appsink)
+pipeline.add(nv_tee)
 
 if not Gst.Element.link(src, camera_filter):
     print("1 Elements could not be linked.")
@@ -283,38 +256,42 @@ if not Gst.Element.link(nv_vid_conv, tee):
     print("3 Elements could not be linked.")
     exit(-1)
 
-if not Gst.Element.link(tee, queue_1):
+if not Gst.Element.link(tee, queue_2):
     print("4 Elements could not be linked.")
     exit(-1)
 
-if not Gst.Element.link(queue_1, videoconvert_1):
-    print("4444 Elements could not be linked.")
-    exit(-1)
-
-if not Gst.Element.link(videoconvert_1, appsink):
-    print("1122 Elements could not be linked.")
-    exit(-1)
-
-if not Gst.Element.link(tee, queue_2):
-    print("6 Elements could not be linked.")
-    exit(-1)
-
 if not Gst.Element.link(queue_2, overlay):
-    print("7 Elements could not be linked.")
+    print("5 Elements could not be linked.")
     exit(-1)
 
 if not Gst.Element.link(overlay, h_264_enc):
-    print("11 Elements could not be linked.")
+    print("6 Elements could not be linked.")
     exit(-1)
 
 if not Gst.Element.link(h_264_enc, rtp_264_pay):
-    print("8 Elements could not be linked.")
+    print("7 Elements could not be linked.")
     exit(-1)
 
 if not Gst.Element.link(rtp_264_pay, udp_sink):
+    print("8 Elements could not be linked.")
+    exit(-1)
+
+
+if not Gst.Element.link(tee, queue_1):
     print("9 Elements could not be linked.")
     exit(-1)
 
+if not Gst.Element.link(queue_1, scale):
+    print("10 Elements could not be linked.")
+    exit(-1)
+
+if not Gst.Element.link(scale, videoconvert_1):
+    print("11 Elements could not be linked.")
+    exit(-1)
+
+if not Gst.Element.link(videoconvert_1, appsink):
+    print("12 Elements could not be linked.")
+    exit(-1)
 
 # Start pipeline
 pipeline.set_state(Gst.State.PLAYING)
@@ -322,10 +299,8 @@ pipeline.set_state(Gst.State.PLAYING)
 try:
 
     while True:
-        pass    
+        pass
 
 except KeyboardInterrupt:
     # Stop Pipeline
     pipeline.set_state(Gst.State.NULL)
-
-
